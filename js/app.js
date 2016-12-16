96c888c007ce6302f347ced1e2079674d431c9b5
Roylix <rodrigoparrb@gmail.com>
2016-12-16 00:50:35 -0600

    Modificaciones para Smartfones y Tablets Menus

--- js/app.js
+++ js/app.js
@@ -4,3 +4,17 @@ $('[data-toggle-dia]').click(function (ev){
 	$('#lineup-tabs').foundation('selectTab',panel)
 	// console.log(panel);
 })
+
+//guardar en una variable lo que nos devuelve jquery de ese elemento del DOM
+const $offCanvas = $('#offCanvas')
+const $sticky = $('#sticky')
+const $drilldown = $('[data-drilldown]') 
+// Para hacer que despues de seleccionar una opcion se borre cuando volvamos a hacer click en menu
+
+$offCanvas.find('li').click(function (ev) {
+	$drilldown.foundation('_hideAll')
+	$offCanvas.foundation('close')
+	setTimeout(function(){
+		$sticky.css('left', '0px')
+	},300)
+})
